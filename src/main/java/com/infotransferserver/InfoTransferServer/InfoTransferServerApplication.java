970a860f749e0200192db78ef01435c0338c80a2
koczkak98 <69437695+koczkak98@users.noreply.github.com>
2020-12-30 00:50:05 +0100

    add tomcat server

--- src/main/java/com/infotransferserver/InfoTransferServer/InfoTransferServerApplication.java
+++ src/main/java/com/infotransferserver/InfoTransferServer/InfoTransferServerApplication.java
@@ -2,12 +2,13 @@ package com.infotransferserver.InfoTransferServer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 import javax.annotation.PostConstruct;
 import java.util.TimeZone;
 
 @SpringBootApplication
-public class InfoTransferServerApplication {
+public class InfoTransferServerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InfoTransferServerApplication.class, args);
